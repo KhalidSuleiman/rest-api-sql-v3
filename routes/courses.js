ec5471e91dbd9139fc2549dcdfae4f818822d94c
KhalidNabhani <khalid.suleiman.hemyer@gmail.com>
2022-01-18 19:51:49 +0400

    Created All Courses Routes and successfully  tested

--- routes/courses.js
+++ routes/courses.js
@@ -19,35 +19,95 @@ router.get('/courses', asyncHandler(async (req, res) => {
           ],
           attributes: {exclude: ['createdAt','updatedAt']}            
     
-}
-   
-        //{ 
-       // include: [{ model: User, 
-                   // as: 'userInfo',
-                    //attributes: {exclude: ['password,createdAt','updatedAt']} 
-                //}] 
-                //}
-                ); 
-    console.log(courses.map(course => course.get({ plain: true })));   
+}); 
+    // console.log(courses.map(course => course.get({ plain: true })));   
     res.json(courses);
 }));
 
 
-router.get('/courses/:id', asyncHandler((req, res, next)=>{
+router.get('/courses/:id', asyncHandler( async (req, res, next)=>{
+    const courses = await Course.findByPk( req.params.id, {
+        include: [
+            {
+              model: User,
+              as: 'userInfo',
+              attributes: {exclude: ['password','createdAt','updatedAt']}, 
+              
+            }
+          ],
+          attributes: {exclude: ['createdAt','updatedAt']}            
     
-
+});   
+res.json(courses);
 }))
 
-router.post('/courses',authenticateUser, asyncHandler((req, res, next)=>{
+router.post('/courses',authenticateUser, asyncHandler( async (req, res, next)=>{
+   
+    try{
+        
+        const course = await Course.create(req.body);
+        res.location('/');
+        res.status(201).json(course);
+
+    }catch(err){
+       if (
+        err.name === 'SequelizeValidationError' ||
+        err.name === 'SequelizeUniqueConstraintError'
+      ) {
+        const errors = err.errors.map((errs) => errs.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      }
+    }
+ 
+}));
+
+router.put('/courses/:id',authenticateUser, asyncHandler( async (req, res, next)=>{
+    const course = await Course.findByPk( req.params.id)
+    
+    try{
+        
+        await course.update(req.body);
+        // res.location('/');
+        // res.status(204).json(updatedCourse );
+        res.status(204);
+
+    }catch(err){
+       if (
+        err.name === 'SequelizeValidationError' ||
+        err.name === 'SequelizeUniqueConstraintError'
+      ) {
+        const errors = err.errors.map((errs) => errs.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      }
+    }
     
 }))
 
-router.put('/courses/:id',authenticateUser, asyncHandler((req, res, next)=>{
+router.delete('/courses/:id',authenticateUser, asyncHandler(async (req, res, next)=>{
+    const course = await Course.findByPk( req.params.id)
     
-}))
+    try{
+        
+        await course.destroy();
+        // res.location('/');
+        // res.status(204).json(updatedCourse );
+        res.status(204);
 
-router.delete('/courses/:id',authenticateUser, asyncHandler((req, res, next)=>{
-    
+    }catch(err){
+       if (
+        err.name === 'SequelizeValidationError' ||
+        err.name === 'SequelizeUniqueConstraintError'
+      ) {
+        const errors = err.errors.map((errs) => errs.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      }
+    }
 }))
 
 
